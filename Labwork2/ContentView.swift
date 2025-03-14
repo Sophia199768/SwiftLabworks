0d0d92391f3d159c5460121eac77a01dd04de873
sophia <sofiya.brovkina@inbox.ru>
2025-03-14 20:30:35 +0300

    Initial Commit

---
+++ Labwork2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Labwork2
+//
+//  Created by Sophia on 14.03.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Labwork2/Labwork2App.swift
@@ -0,0 +1,17 @@
+//
+//  Labwork2App.swift
+//  Labwork2
+//
+//  Created by Sophia on 14.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct Labwork2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
